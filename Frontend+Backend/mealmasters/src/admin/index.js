e7fdc2d215dae6c6508dd6066088cecedf1af1d2
Atharv Dabhade <atharvdabhade101@gmail.com>
2026-02-03 10:56:07 +0530

    backend

--- Frontend+Backend/mealmasters/src/admin/index.js
+++
@@ -1,2 +0,0 @@
-export { default as AdminDrawer } from "./navigation/AdminDrawer";
-export { default as AdminEntry } from "./navigation/AdminEntry";
--- Frontend+Backend/mealmasters/src/services/config.js
+++
@@ -1,4 +0,0 @@
-export const config = {
-  url: 'http://192.168.1.45:4000'
-  // url: 'http://192.168.1.45:4000'
-}
