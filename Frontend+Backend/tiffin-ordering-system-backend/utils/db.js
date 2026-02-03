39240f1f34eb389f5c7598a258cba148c736149a
Atharv Dabhade <atharvdabhade101@gmail.com>
2026-02-03 12:23:10 +0530

    deploying

--- Frontend+Backend/tiffin-ordering-system-backend/utils/db.js
+++ Frontend+Backend/tiffin-ordering-system-backend/utils/db.js
@@ -1,10 +1,13 @@
 const mysql2 = require('mysql2')
 
-const pool=mysql2.createPool({
-    host : 'localhost',
-    user : 'W2_92912_Atharv',
-    password : 'manager',
-    database : 'onlinetiffinsystem_db'
-})
+
+
+const pool = mysql2.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306
+});
 
 module.exports=pool
