bfa0fda372dbac9c7ff10c2b93e90301f0a87383
Atharv Dabhade <atharvdabhade101@gmail.com>
2026-02-03 15:19:09 +0530

    backend changes

--- Frontend+Backend/tiffin-ordering-system-backend/server.js
+++ Frontend+Backend/tiffin-ordering-system-backend/server.js
@@ -45,7 +45,7 @@
 require('dotenv').config();
 const express = require('express')
 const cors = require('cors')
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 
 const authorizeUser = require('./authorization/authuser')
